1a8876a69ae1ac0a70a711710e15d63ac333a9be
douglasskiser <douglass.kiser@me.com>
2016-09-28 15:34:02 -0500

    adding authorization token to allowed headers

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Authorizationtoken, Content-Length, X-Requested-With, X-HTTP-Method-Override');
     res.send(200);
   }
   else {
