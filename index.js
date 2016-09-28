0babda68b7b700eb2d142e31c4239c6ae471220c
douglasskiser <douglass.kiser@me.com>
2016-09-28 15:20:57 -0500

    add CORS handler to express

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ var googleMapsClient = require('@google/maps').createClient({
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(function(req, res, next) {
+  if ('OPTIONS' == req.method) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+
 app.get('/autocomplete/:input', function (req, res) {
   googleMapsClient.placesAutoComplete({
     input: req.params.input || ''
