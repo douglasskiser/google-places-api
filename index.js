623fe39ad5d7fc82262bda13e1ad2eb8e100c024
douglasskiser <douglass.kiser@me.com>
2016-09-28 15:30:46 -0500

    update CORS handler

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(function(req, res, next) {
   if ('OPTIONS' == req.method) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override');
     res.send(200);
   }
   else {
